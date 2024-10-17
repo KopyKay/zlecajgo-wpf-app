ac511a04ad28b60af1d0d726df73176d0ac997bc
KopyKay <124162537+KopyKay@users.noreply.github.com>
2024-10-17 16:53:58 +0200

    Create wpf project from template

---
+++ ZlecajGoWpfApp/ZlecajGoWpfApp/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace ZlecajGoWpfApp;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
+{
+}
