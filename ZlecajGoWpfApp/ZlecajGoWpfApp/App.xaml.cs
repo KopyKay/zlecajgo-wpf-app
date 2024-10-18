dc69f8c1c881a62e7b7f0a01586b7a3accb27417
KopyKay <124162537+KopyKay@users.noreply.github.com>
2024-10-18 16:44:46 +0200

    Added dependency injection and host setup

--- ZlecajGoWpfApp/ZlecajGoWpfApp/App.xaml.cs
+++ ZlecajGoWpfApp/ZlecajGoWpfApp/App.xaml.cs
@@ -1,12 +1,37 @@
-﻿using System.Configuration;
-using System.Data;
-using System.Windows;
+﻿using System.Windows;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 
 namespace ZlecajGoWpfApp;
 
-/// <summary>
-/// Interaction logic for App.xaml
-/// </summary>
 public partial class App : Application
 {
+    public static IHost AppHost { get; private set; }
+
+    public App()
+    {
+        AppHost = Host.CreateDefaultBuilder()
+            .ConfigureServices((hostContext, services) =>
+            {
+                services.AddSingleton<MainWindow>();
+            })
+            .Build();
+    }
+
+    protected override async void OnStartup(StartupEventArgs e)
+    {
+        await AppHost.StartAsync();
+
+        var startupWindow = AppHost.Services.GetRequiredService<MainWindow>();
+        startupWindow.Show();
+        
+        base.OnStartup(e);
+    }
+
+    protected override async void OnExit(ExitEventArgs e)
+    {
+        await AppHost.StopAsync();
+        
+        base.OnExit(e);
+    }
 }
