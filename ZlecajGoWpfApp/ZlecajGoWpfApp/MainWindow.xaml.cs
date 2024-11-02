0fd13fa43789ab92d247efb28553d73a7ea7e11f
KopyKay <124162537+KopyKay@users.noreply.github.com>
2024-11-02 18:15:04 +0100

    Moved snackbar to MainWindow class
    The snackbar is now a single object within the MainWindow, rather than multiple instances on each page.

--- ZlecajGoWpfApp/ZlecajGoWpfApp/MainWindow.xaml.cs
+++ ZlecajGoWpfApp/ZlecajGoWpfApp/MainWindow.xaml.cs
@@ -1,12 +1,14 @@
 ﻿using System.Windows;
+using ZlecajGoWpfApp.Services;
 using ZlecajGoWpfApp.Views;
 
 namespace ZlecajGoWpfApp;
 
 public partial class MainWindow : Window
 {
-    public MainWindow()
+    public MainWindow(SnackbarService snackbarService)
     {
         InitializeComponent();
+        MainSnackbar.MessageQueue = snackbarService.MessageQueue;
     }
 }
